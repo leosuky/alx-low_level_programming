9334ab23e412edf0a668250cb7243f13a08eca5f
Sunkanmi Otitolaye <leosuky99@gmail.com>
2022-09-08 14:51:00 -0700

    Should work now...

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,7 @@ int main(void)
 	char str[] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
 
 	fwrite(str,
-	      2,
-	      sizeof(str));
+	      sizeof(str), 1, stderr);
 	return (1);
 }
 
